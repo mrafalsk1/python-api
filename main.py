5823481bfea408cb62965c7c4e93b5710b03fe2f
Matheus Rafalski <matheus.rafalski@nuvoni.com.br>
2023-08-16 17:49:19 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+import uvicorn
+
+
+app = FastAPI()
+
+if __name__ == '__main__':
+    uvicorn.run(app, port=8080, host='0.0.0.0')
+
+@app.get('/')
+async def root():
+    return {'example': 'example'}
